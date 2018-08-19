8fd4eba3d5c6936a6122dfb2c3bc47e3a06c7c51
David Knopp <daknopp@gmail.com>
2018-08-18 21:22:10 -0700

    Added unity project

---
+++ Juno/Assets/Scripts/Juno/Runtime/Container/DIContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Juno
+{
+    public class DIContainer
+    {
+        #region public
+        #endregion // public
+
+        #region private
+        private Dictionary<Type, Dictionary<int, List<object>>> m_bindings;
+        #endregion // private
+    }
+}
