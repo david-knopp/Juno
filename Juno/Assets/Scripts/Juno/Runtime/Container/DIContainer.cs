5cb8201c41f60ae0060613a9112b92291f1382a9
David Knopp <daknopp@gmail.com>
2018-08-25 13:45:30 -0700

    Added anonymous getters

--- Juno/Assets/Scripts/Juno/Runtime/Container/DIContainer.cs
+++ Juno/Assets/Scripts/Juno/Runtime/Container/DIContainer.cs
@@ -7,18 +7,11 @@ namespace Juno
     public class DIContainer
     {
         #region public
-        public int AnonymousID
-        {
-            get;
-            set;
-        }
-
-        public DIContainer( int anonymousID = int.MinValue )
+        public DIContainer()
         {
             m_namedBindings = new Dictionary<Type, Dictionary<int, object>>();
             m_anonymousBindings = new Dictionary<Type, List<object>>();
             m_injectQueue = new HashSet<object>();
-            AnonymousID = anonymousID;
         }
 
         #region binding
@@ -48,7 +41,7 @@ namespace Juno
             bindings.Clear();
         }
 
-        public bool TryGet<T>( out T instance )
+        public bool TryGetFirst<T>( out T instance )
         {
             var bindings = GetAnonymousBindings( typeof( T ) );
             if ( bindings.Count > 0 )
@@ -63,15 +56,40 @@ namespace Juno
             }
         }
 
-        public T Get<T>()
+        public T GetFirst<T>()
         {
             T instance;
-            if ( TryGet( out instance ) )
+            if ( TryGetFirst( out instance ) )
             {
                 return instance;
             }
 
-            throw new KeyNotFoundException( string.Format( "DIContainer.Get: No anonymous bindings exist for type '{0}'", typeof( T ).FullName ) );
+            throw new InvalidOperationException( string.Format( "DIContainer.GetFirst: No anonymous bindings exist for type '{0}'", typeof( T ).FullName ) );
+        }
+
+        public bool TryGet<T>( out List<T> instances )
+        {
+            List<object> bindings;
+
+            if ( m_anonymousBindings.TryGetValue( typeof( T ), out bindings ) )
+            {
+                instances = bindings.Cast<T>().ToList();
+                return true;
+            }
+
+            instances = default( List<T> );
+            return false;
+        }
+
+        public List<T> Get<T>()
+        {
+            List<T> instances;
+            if ( TryGet( out instances ) )
+            {
+                return instances;
+            }
+
+            throw new InvalidOperationException( string.Format( "DIContainer.Get: No anonymous bindings exist for type '{0}'", typeof( T ).FullName ) );
         }
         #endregion // anonymous
 
@@ -128,6 +146,20 @@ namespace Juno
             throw new KeyNotFoundException( string.Format( "DIContainer.Get: No bindings exist for type '{0}' with id '{1}'", typeof( T ).FullName, id ) );
         }
         #endregion // named
+
+        public List<T> GetAll<T>()
+        {
+            Dictionary<int, object> namedBindings = GetNamedTypeBindings( typeof( T ) );
+            List<T> bindings = namedBindings.Values.Cast<T>().ToList();
+
+            List<object> anonymousBindings;
+            if ( m_anonymousBindings.TryGetValue( typeof( T ), out anonymousBindings ) )
+            {
+                bindings.AddRange( anonymousBindings.Cast<T>() );
+            }
+            
+            return bindings;
+        }
         #endregion // binding
 
         #region injection
