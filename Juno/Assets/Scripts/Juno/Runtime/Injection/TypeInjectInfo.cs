6472487ef3edd494e791ad21b1afc222c5a18b82
David Knopp <daknopp@gmail.com>
2018-09-04 22:34:43 -0700

    Added injection classes

---
+++ Juno/Assets/Scripts/Juno/Runtime/Injection/TypeInjectInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Juno
+{
+    internal struct TypeInjectInfo
+    {
+        public TypeInjectInfo( List<MethodInjectInfo> methodInfo )
+        {
+            MethodInfo = methodInfo;
+        }
+
+        public List<MethodInjectInfo> MethodInfo
+        {
+            get;
+            private set;
+        }
+    }
+}
