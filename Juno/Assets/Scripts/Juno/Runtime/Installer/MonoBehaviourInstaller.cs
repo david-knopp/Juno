8c6f9c5d8991503586f06f9ebf2275fad8353e0e
David Knopp <daknopp@gmail.com>
2018-09-16 14:09:58 -0700

    Added initial context and installer work

---
+++ Juno/Assets/Scripts/Juno/Runtime/Installer/MonoBehaviourInstaller.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Juno
+{
+    public abstract class MonoBehaviourInstaller : MonoBehaviour, IInstaller
+    {
+        public abstract void InstallBindings( DIContainer container );
+    }
+}
