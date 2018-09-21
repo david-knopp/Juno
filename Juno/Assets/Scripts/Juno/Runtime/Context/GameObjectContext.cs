e43ff94c0d6e0a88a8004cfebc4f0e89bc35f82a
David Knopp <daknopp@gmail.com>
2018-09-20 21:34:32 -0700

    Fixed bug with injection utils, added initial work for GameObjectContexts

--- Juno/Assets/Scripts/Juno/Runtime/Context/GameObjectContext.cs
+++ Juno/Assets/Scripts/Juno/Runtime/Context/GameObjectContext.cs
@@ -1,6 +1,35 @@
-﻿namespace Juno
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Juno
 {
     public class GameObjectContext : ContextBase
     {
+        #region protected
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+
+            // TODO: Try to parent to scene first
+            Container.Parent = ProjectContext.Instance.Container;
+        }
+
+        protected override void OnPreInject()
+        {
+            base.OnPreInject();
+            QueueSceneObjectsForInject();
+        }
+        #endregion // protected
+
+        #region private
+        private void QueueSceneObjectsForInject()
+        {
+            List<MonoBehaviour> behaviors = InjectUtils.GetInjectableMonoBehaviours( gameObject );
+            foreach ( var behavior in behaviors )
+            {
+                Container.QueueForInject( behavior );
+            }
+        }
+        #endregion // private
     }
 }
