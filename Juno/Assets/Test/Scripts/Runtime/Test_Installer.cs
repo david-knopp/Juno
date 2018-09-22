bdab5eca3b0b784212f5b1bb788c130dacd2206b
David Knopp <daknopp@gmail.com>
2018-09-21 22:07:04 -0700

    Fixed bug with contexts not properly checking their parents

---
+++ Juno/Assets/Test/Scripts/Runtime/Test_Installer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Juno.Test
+{
+    public class Test_Installer : MonoBehaviourInstaller
+    {
+        public override void InstallBindings( DIContainer container )
+        {
+            container.Bind( m_value );
+        }
+
+
+        [SerializeField] private string m_value;
+    }
+}
