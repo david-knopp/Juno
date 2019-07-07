f6558fa1493eac3fd220cf637b96bd7f2697ec98
David Knopp <daknopp@gmail.com>
2019-07-07 12:37:16 -0700

    Added ability to bind with interfaces, and added initialize, dispose, and tickable interface support

---
+++ Juno/Assets/Test/Scripts/Runtime/Test_Interfaces.cs
@@ -0,0 +1,29 @@
+﻿using Juno;
+using System;
+using UnityEngine;
+
+namespace Juno
+{
+    public sealed class Test_Interfaces : IInitializable, IDisposable, ITickable, ILateTickable
+    {
+        public void Initialize()
+        {
+            Debug.Log( "Initialize" );
+        }
+
+        public void Dispose()
+        {
+            Debug.Log( "Dispose" );
+        }
+
+        public void Tick()
+        {
+            Debug.Log( "Tick" );
+        }
+
+        public void LateTick()
+        {
+            Debug.Log( "LateTick" );
+        }
+    }
+}
